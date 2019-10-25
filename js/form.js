91e5da9eb526ba5f6a4ac10dc8c61938a2d7f2d0
lilian ngoiri <lillianngoiri67@gmail.com>
2019-10-25 11:28:51 +0300

    Added a background image for the form

---
+++ js/form.js
@@ -0,0 +1,6 @@
+var month = document.getElementById("month").value;
+var MM = parseInt(month);
+var day = document.getElementById("day").value;
+var DD = parseInt(day); 
+var year = document.getElementById("year").value;
+var YY = parseInt(year);
