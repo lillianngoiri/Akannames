be4a84d73e46583b78911516da08e9723a53ed4f
lilian ngoiri <lillianngoiri67@gmail.com>
2019-10-25 12:38:37 +0300

    Added arrays using javascript

--- js/form.js
+++ js/form.js
@@ -4,3 +4,14 @@ var day = document.getElementById("day").value;
 var DD = parseInt(day); 
 var year = document.getElementById("year").value;
 var YY = parseInt(year);
+
+var maleNames = [
+     "Sunday: Kwasi","Monday:Kwadwo","Tuesday:Kwabena","Wednesday:Kwaku","Thursday:Yaw","Friday:Kofi","Saturday: Kwame"];
+
+
+var femaleNames = [
+     "Sunday:Akosua", "Monday:Adwoa","Tuesday:Abenaa","Wednesday:Akua","Thursday:Yaa","Friday:Afua","Saturday:Ama"];
+
+
+
+
