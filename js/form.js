f5324c46be544f7c98b4bd4a5fa73f28256ddd67
lilian ngoiri <lillianngoiri67@gmail.com>
2019-10-26 15:32:19 +0300

    made some changes on the formula that i used

--- js/form.js
+++ js/form.js
@@ -1,18 +1,31 @@
-var month = document.getElementById("month").value;
-var MM = parseInt(month);
-var day = document.getElementById("day").value;
-var DD = parseInt(day); 
-var year = document.getElementById("year").value;
-var YY = parseInt(year);
-var centuary = document.getElementById("centuary").value;
-var CC = parseInt(centuary);
+function gh() {
+    var month = document.getElementById("month").value;
+    var MM = parseInt(month);
+    var day = document.getElementById("day").value;
+    var DD = parseInt(day);
+    var year = document.getElementById("year").value;
+    var YY = parseInt(year);
+    var centuary = document.getElementById("centuary").value;
+    var CC = parseInt(centuary);
 
-var maleNames = [
-     "Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
+    var maleNames = [
+        "Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+
+        
+    var femaleNames = [
+        "Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+
+    var day = ( ( (CC/4) - (2 * CC - 1)) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) %7;
+     day = Math.round(day);
+    var dob = new Date(CC+""+YY+"-"+MM+"-"+DD);
+      day = dob.getDay();
 
 
-var femaleNames = [
-     "Akosua", "Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
+    if (document.getElementById("male").checked) {
+        alert("Your Akan name is " + maleNames[day]);
+    }
+    else if (document.getElementById("female").checked) {
+        alert("Your Akan name is " + femaleNames[day]);
+    }
 
-var day = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7
-
+}
