4bc250dae1fc0f1cb836146726bedda1f50c3d43
lilian ngoiri <lillianngoiri67@gmail.com>
2019-10-29 14:50:00 +0300

    added some validation

--- js/form.js
+++ js/form.js
@@ -18,10 +18,11 @@ function gh() {
     var day = parseInt(((CC / 4) - 2 * CC - 1) + ((5 * YY / 4)) + ((26 * (MM + 1) / 10)) + DD) % 7;
      day = Math.round(day);
 
-     if (MM < 1 || MM > 12 || MM == 2 && DD > 29) {
+
+      if (MM !=="" || MM < 1 || MM > 12 || MM == 2 && DD > 29) {
             alert("INVALID MONTH!!!");
         }
-        else if (DD < 1 || DD > 31) {
+      if (DD !=="" || DD < 1 || DD > 31) {
             alert("INVALID DAY!!!");
         }
 
